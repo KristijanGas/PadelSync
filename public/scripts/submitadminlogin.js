d55d18699f118959f7c4b85725923e94f22d9291
K <kristijan.gaspar.fer.unizg.hr>
2025-10-08 15:19:16 +0200

    Added preparation for login form where OAUTH will be implemented

---
+++ public/scripts/submitadminlogin.js
@@ -0,0 +1,10 @@
+
+
+function submitAdminLoginForm() {
+    // Get form values
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    //IMPLEMENT OAUTH HERE AND SEND TO BACKEND
+    alert("Admin login submitted: " + username);
+}
---
+++ public/scripts/submituserlogin.js
@@ -0,0 +1,10 @@
+
+
+function submitUserLoginForm() {
+    // Get form values
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    //IMPLEMENT OAUTH HERE AND SEND TO BACKEND
+    alert("User login submitted: " + username);
+}
