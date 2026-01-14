251973d8fc678a0ec5b683e194fb790c6963af47
andjela-mil <am55730@fer.hr>
2026-01-14 15:03:56 +0100

    socket

--- routes/terrain.routes.js
+++ routes/terrain.routes.js
@@ -288,8 +288,7 @@ router.post('/:id', requiresAuth(), async (req, res) => {
 
     const currentUrl = `${req.protocol}://${req.get('host')}`;
     if(tipPlacanja==="gotovina"){
-      sendNotificationFromTemplate("zahtjevZaRezervacijomKlub", teren.username, username, datum, termin.vrijemePocetak, termin.vrijemeKraj, teren.terenID, teren.imeTeren);
-      res.json({redirect : `${currentUrl}/terrain/${teren.terenID}`});
+       res.json({redirect : `${currentUrl}/terrain/${teren.terenID}`});
     }else{
       const url = req.protocol + "://" + req.headers.host;
       res.json({checkoutUrl : `${url}/stripe/payment`, transakcijaID});
