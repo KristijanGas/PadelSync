316e7d1173a3e28b09207572c5a2597016efa34d
Lara1355 <lara.lovrincevic1@gmail.com>
2026-01-15 22:22:49 +0100

    dodavanje css i sredivanje inboxa

--- public/scripts/inbox.js
+++ public/scripts/inbox.js
@@ -1,13 +1,27 @@
 const messages = document.querySelectorAll(".messageTitle");
 
-
-messages.forEach(message =>{
+messages.forEach(message => {
     message.addEventListener("click", async () => {
         const url = `/myInbox/read/${message.id}`;
         const res = await fetch(url, {
             method: "GET",
             headers: { "Content-Type": "application/json" }
         });
-        //lara treba dodati "proširenje obavijesti tu"
-    })
-})
+
+        const content = message.nextElementSibling;
+
+        if (content.style.maxHeight && content.style.maxHeight !== "0px") {
+            content.style.maxHeight = "0px";
+            content.style.opacity = 0;
+            content.style.paddingTop = 0;
+            content.style.paddingBottom = 0;
+            content.style.marginTop = 0;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+            content.style.opacity = 1;
+            content.style.paddingTop = "10px";
+            content.style.paddingBottom = "10px";
+            content.style.marginTop = "10px";
+        }
+    });
+});
